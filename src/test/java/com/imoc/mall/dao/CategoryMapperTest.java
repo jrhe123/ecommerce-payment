a104ef6db3573753c14eb5848a350cf90ae6c7cd
jrhe1213 <jiarong1213@gmail.com>
2022-03-19 17:42:46 -0400

    setup junit test

---
+++ src/test/java/com/imoc/mall/dao/CategoryMapperTest.java
@@ -0,0 +1,29 @@
+package com.imoc.mall.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.imoc.mall.pojo.Category;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryMapperTest {
+	
+	@Autowired
+	private CategoryMapper categoryMapper;
+
+	@Test
+	public void findById() {
+		Category category = categoryMapper.findById(1);
+		System.out.println(category.getName());
+	}
+
+	@Test
+	public void queryById() {
+		Category category = categoryMapper.queryById(1);
+		System.out.println(category.getName());
+	}
+}
